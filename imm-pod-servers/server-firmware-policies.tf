647cab58e564e24b6697963fb9963697027373a4
bywhite <bywhite@cisco.com>
2023-12-11 16:24:26 -0600

    updated firmware policy

--- imm-pod-servers/server-firmware-policies.tf
+++ imm-pod-servers/server-firmware-policies.tf
@@ -10,10 +10,7 @@ resource "intersight_firmware_policy" "server_firmware_x210c" {
  #may need variables for power_priority  and use var in name "med"
   name        = "${var.server_policy_prefix}-server-firmware-x210c"
   description              = var.description
-  model_bundle_combo {
-    bundle_version = "5.2(0.230092)"
-    model_family   = "UCSX-210C-M6"
-  }
+
   model_bundle_combo {
     bundle_version = "5.2(0.230092)"
     model_family   = "UCSX-210C-M7"
